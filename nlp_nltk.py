f1725294e98f89308312c75f61949295da12251a
Ji Su Lee <32809393+LogSigma@users.noreply.github.com>
2018-05-29 20:04:12 +0900

    Update nlp_nltk.py

--- nlp_nltk.py
+++ nlp_nltk.py
@@ -1,5 +1,6 @@
 from konlpy.tag import Twitter
 import nltk
+
 twitter = Twitter()
 
 def read_data(filename):
@@ -13,23 +14,25 @@ def tokenize(doc):
 def term_exists(doc):
   return {'exists({})'.format(word): (word in set(doc)) for word in selected_words}
   
+# 데이터 읽기
 train_data = read_data('data/ratings_train.txt')
 test_data = read_data('data/ratings_test.txt')
 
-## ㅎㅕㅇㅌㅐㅅㅗ ㅂㅜㄴㄹㅠ
+# 형태소 분류
 train_docs = [(tokenize(row[1]), row[2]) for row in train_data[1:]]
 test_docs = [(tokenize(row[1]), row[2]) for row in test_data[1:]]
 
+# Training 데이터 token 수집
 tokens = [t for d in train_docs for t in d[0]]
 text = nltk.Text(tokens, name='NMSC')
 
+# trem의 따른 분류
 selected_words = [f[0] for f in text.vocab().most_common(2000)]
-
 train_xy = [(term_exists(d), c) for d, c in train_docs]
 test_xy = [(term_exists(d), c) for d, c in test_docs]
 
+# nltk의 NaiveBayesClassifier 결과 확인
 classifier = nltk.NaiveBayesClassifier.train(train_xy)
 print(nltk.classify.accuracy(classifier, test_xy))
-# -> 0.
 
 classifier.show_most_informative_features(10)
