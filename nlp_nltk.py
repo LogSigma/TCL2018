20ff985f84ce3c6c26aba9efd8570543ee3322a5
Ji Su Lee <32809393+LogSigma@users.noreply.github.com>
2018-06-05 13:30:07 +0900

    Update nlp_nltk.py

--- nlp_nltk.py
+++ nlp_nltk.py
@@ -1,35 +1,30 @@
-from konlpy.tag import Twitter
 import nltk
+import pandas as pd
+from konlpy.tag import Twitter
 
 twitter = Twitter()
+feature_count=2000
 
-def read_data(filename):
-  with open(filename, 'r') as f:
-    data = [line.split('\t') for line in f.read().splitlines()]
-  return data
+# 1. 
 
-def tokenize(doc):
-  return ['/'.join(t) for t in twitter.pos(doc, norm=True, stem=True)]
-
-def term_exists(doc):
-  return {'exists({})'.format(word): (word in set(doc)) for word in selected_words}
-  
 # 데이터 읽기
-train_data = read_data('data/ratings_train.txt')
-test_data = read_data('data/ratings_test.txt')
+train_data = pd.read_scv('data/ratings_train.txt', sep='\t', encoding='CP949')
+test_data = pd.read_scv('data/ratings_test.txt', sep='\t', encoding='CP949')
 
 # 형태소 분류
-train_docs = [(tokenize(row[1]), row[2]) for row in train_data[1:]]
-test_docs = [(tokenize(row[1]), row[2]) for row in test_data[1:]]
+train_docs = [(['/'.join(t) for t in twitter.pos(row[2], norm=True, stem=True)], row[3]) for row in train_data.itertuples()]
+test_docs = [(['/'.join(t) for in twitter.pos(row[2], norm=True, stem=True)], row[3]) for row in test_data.itertuples()]
 
 # Training 데이터 token 수집
 tokens = [t for d in train_docs for t in d[0]]
+
 text = nltk.Text(tokens, name='NMSC')
+print(text.vocab().most_common(10))
 
 # trem의 따른 분류
-selected_words = [f[0] for f in text.vocab().most_common(2000)]
-train_xy = [(term_exists(d), c) for d, c in train_docs]
-test_xy = [(term_exists(d), c) for d, c in test_docs]
+selected_words = [f[0] for f in text.vocab().most_common(feature_count)]
+train_xy = [({'exists({})'.format(word): (word in set(d)) for word in selected_words}, c) for d, c in train_data]
+test_xy = [({'exists({})'.format(word): (word in set(d)) for word in selected_words}, c) for d, c in test_data]
 
 # nltk의 NaiveBayesClassifier 결과 확인
 classifier = nltk.NaiveBayesClassifier.train(train_xy)
