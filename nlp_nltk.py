e9ef1929644525cfa4de21f40317f2b88a39b36d
Ji Su Lee <32809393+LogSigma@users.noreply.github.com>
2018-06-05 13:34:00 +0900

    Update nlp_nltk.py

--- nlp_nltk.py
+++ nlp_nltk.py
@@ -12,11 +12,11 @@ train_data = pd.read_scv('data/ratings_train.txt', sep='\t', encoding='CP949')
 test_data = pd.read_scv('data/ratings_test.txt', sep='\t', encoding='CP949')
 
 # 형태소 분류
-train_docs = [(['/'.join(t) for t in twitter.pos(row[2], norm=True, stem=True)], row[3]) for row in train_data.itertuples()]
-test_docs = [(['/'.join(t) for in twitter.pos(row[2], norm=True, stem=True)], row[3]) for row in test_data.itertuples()]
+train_data = [(['/'.join(t) for t in twitter.pos(row[2], norm=True, stem=True)], row[3]) for row in train_data.itertuples()]
+test_data = [(['/'.join(t) for t in twitter.pos(row[2], norm=True, stem=True)], row[3]) for row in test_data.itertuples()]
 
 # Training 데이터 token 수집
-tokens = [t for d in train_docs for t in d[0]]
+tokens = [t for d in train_data for t in d[0]]
 
 text = nltk.Text(tokens, name='NMSC')
 print(text.vocab().most_common(10))
